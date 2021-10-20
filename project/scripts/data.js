265eae1e79f87c60be255ae8eb6e2dc42ed6a3cc
mikita.bahamazau <ereticor@gmail.com>
2021-10-20 18:50:38 +0300

    feat: add article data file

---
+++ project/scripts/data.js
@@ -0,0 +1,108 @@
+const articles = [
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/1.png',
+    keywords: ['angular', ],
+  },
+  {
+    title: 'How to RxJS in Angular',
+    summary: `As the values of your component 
+      inputs change over time, you may want to do 
+      something with that data inside your component.`,
+    src: './assets/images/articles/2.png',
+    keywords: ['angular', ],
+  },
+  {
+    title: 'Top 15 Features of Angular',
+    summary: `It has added the information for the dependency 
+      and also regarding the ng-content selections for the data.`,
+    src: './assets/images/articles/3.png',
+    keywords: ['angular', ],
+  },
+  {
+    title: 'Angular team streamlines feature requests',
+    summary: `Feature requests will be reviewed for 
+      alignment with existing projects on the Angular roadmap.`,
+    src: './assets/images/articles/4.png',
+    keywords: ['angular', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/5.png',
+    keywords: ['react', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/6.png',
+    keywords: ['react', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/7.png',
+    keywords: ['react', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/8.png',
+    keywords: ['react', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/9.png',
+    keywords: ['java', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/10.png',
+    keywords: ['java', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/11.png',
+    keywords: ['java', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/12.png',
+    keywords: ['java', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/13.png',
+    keywords: ['python', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/14.png',
+    keywords: ['python', ],
+  },
+  {
+    title: 'High quality',
+    summary: `Straightforward APIs with 
+    consistent cross platform behaviour..`,
+    src: './assets/images/articles/15.png',
+    keywords: ['python', ],
+  },
+]
