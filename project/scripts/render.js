647def236f73c65bd58d46617f7cf3912f9beda2
mikita.bahamazau <ereticor@gmail.com>
2021-10-28 19:46:15 +0300

    feat: add log out functionality

--- project/scripts/render.js
+++ project/scripts/render.js
@@ -59,13 +59,22 @@ function createLoginHeader() {
 
   logInWrapper.innerHTML = `
     <button class="head__btn login__create">Create a Post</button>
-    <img class="login__user" src="${userImg}">
+    <img class="login__user" src="${userImg}" title="click to log out">
   `
 
   let postBtn = logInWrapper.querySelector('.login__create')
 
   postBtn.addEventListener('click', () => {
     window.location = 'article.html#createPost'
+  })
+
+  let userProfile = logInWrapper.querySelector('.login__user')
+
+  userProfile.addEventListener('click', () => {
+    localStorage.removeItem('logged')
+    setTimeout(() => {
+      window.location.reload()
+    }, 500)
   })
 
   head.append(logInWrapper)
