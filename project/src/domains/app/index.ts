365ad1f520a1c0c508b4ec5c256fbe7ea68dc7d1
mikita.bahamazau <ereticor@gmail.com>
2021-11-06 14:45:54 +0300

    feat: add selectors for tags

---
+++ project/src/domains/app/index.ts
@@ -0,0 +1,6 @@
+import ActionTypes from "./actionTypes";
+import * as ActionCreators from "./actions";
+import reducer from "./reducer";
+import * as selectors from "./selectors";
+
+export { ActionTypes, ActionCreators, reducer, selectors };
---
+++ project/src/domains/app/selectors.ts
@@ -0,0 +1,10 @@
+import { ReduxStore } from "../../reducers";
+import { createSelector } from "reselect";
+
+export const getApp = (state: ReduxStore) => {
+  return state.app;
+};
+
+export const getTags = createSelector(getApp, (app) => app.tags);
+
+export const isLoadingTags = createSelector(getApp, (app) => app.isLoadingTags);
