7361f907d5423dcdb09d5fd52d460102d2978db4
mikita.bahamazau <ereticor@gmail.com>
2021-11-06 17:04:16 +0300

    feat: add history for redux plugin

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import history from "./store/history";
 import App from "./App";
+import { ConnectedRouter } from "connected-react-router";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.querySelector(".app")
 );
--- project/src/router/index.tsx
+++ project/src/router/index.tsx
@@ -1,10 +1,3 @@
 import Router from "./Router";
-import { connect } from "react-redux";
-import { selectors } from "../domains/auth";
-import { createStructuredSelector } from "reselect";
 
-const selector = createStructuredSelector({
-  isLogged: selectors.isLogged,
-});
-
-export default connect(selector)(Router);
+export default Router;
