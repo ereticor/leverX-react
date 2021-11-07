bec886d1e522ff0565c4a910b64e3733c251b817
mikita.bahamazau <ereticor@gmail.com>
2021-11-07 17:40:13 +0300

    fix: proper path for pages

--- project/src/router/Router.tsx
+++ project/src/router/Router.tsx
@@ -2,10 +2,10 @@ import React, { Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 const Header = React.lazy(() => import("../components/Header"));
 const Home = React.lazy(() => import("../pages/Home"));
-const SearchPage = React.lazy(() => import("../pages/SearchPage"));
-const ArticlePage = React.lazy(() => import("../pages/ArticlePage"));
-const LoginPage = React.lazy(() => import("../pages/LoginPage"));
-const PostPage = React.lazy(() => import("../pages/PostPage"));
+const SearchPage = React.lazy(() => import("../pages/searchPage"));
+const ArticlePage = React.lazy(() => import("../pages/articlePage"));
+const LoginPage = React.lazy(() => import("../pages/loginPage"));
+const PostPage = React.lazy(() => import("../pages/postPage"));
 
 const Router = ({ isLogged }: { isLogged: boolean }) => {
   return (
