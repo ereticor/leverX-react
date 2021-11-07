dc59c894a075e2066c7534962e25bf8e069db1f8
mikita.bahamazau <ereticor@gmail.com>
2021-11-07 17:40:02 +0300

    refactor: delete connectedRouter

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import history from "./store/history";
 import App from "./App";
-import { ConnectedRouter } from "connected-react-router";
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <App />
-    </ConnectedRouter>
+    <App />
   </Provider>,
   document.querySelector(".app")
 );
